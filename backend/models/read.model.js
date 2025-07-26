44887e3ce5a42c696fb34b734c1dafa53a1fa5ff
ishan1999-programmer <ishantripathi16@gmail.com>
2025-07-26 16:56:46 +0530

    Read model created

---
+++ backend/models/read.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const readSchema = new mongoose.Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    bookId: { type: String, required: [true, "BookId is required"] },
+    bookTitle: { type: String, required: [true, "Book title is required"] },
+    bookAuthor: { type: String, required: [true, "Book author is required"] },
+    bookPages: { type: Number, required: [true, "Book pages is required"] },
+    bookRating: {
+      type: Number,
+      required: [true, "Book rating is required"],
+      min: 0,
+      max: 5,
+    },
+    bookGenres: [
+      { type: String, required: [true, "Book genres are required"] },
+    ],
+    status: {
+      type: String,
+      required: [true, "Status is required"],
+      enum: ["want to read", "reading", "read"],
+      default: "want to read",
+    },
+    addedAt: Date,
+    startedAt: Date,
+    completedAt: Date,
+    currentPage: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
