5dd3a107d72381cd692c9056a5052d2005e56917
ishan-cephei <ishan@cepheiinfotech.com>
2026-01-22 14:12:39 +0530

    implemented react-hook-form in login form

--- frontend/src/features/auth/components/LoginForm.jsx
+++ frontend/src/features/auth/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -14,6 +14,18 @@ import { Label } from "@/components/ui/label";
 import "../styles/auth.css";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const handleLoginFormSubmit = async (formData) => {
+    console.log(formData);
+    alert("Success");
+  };
+
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
@@ -22,8 +34,8 @@ const LoginForm = () => {
           Enter your credentials to access your reading community
         </CardDescription>
       </CardHeader>
-      <CardContent>
-        <form onSubmit={(e) => handleLoginSubmit(e)}>
+      <form onSubmit={handleSubmit(handleLoginFormSubmit)}>
+        <CardContent>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
@@ -31,35 +43,60 @@ const LoginForm = () => {
                 id="email"
                 type="email"
                 placeholder="Enter your email"
-                required
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "Please enter a valid email address",
+                  },
+                })}
               />
+              {errors.password && (
+                <p className="text-sm text-red-500">{errors.email?.message}</p>
+              )}
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
               <Input
-                id="email"
-                type="email"
+                id="password"
+                type="password"
                 placeholder="Enter your password"
-                required
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters",
+                  },
+                  pattern: {
+                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
+                    message:
+                      "Password must contain at least one letter and one number",
+                  },
+                })}
               />
+              {errors.password && (
+                <p className="text-sm text-red-500">
+                  {errors.password?.message}
+                </p>
+              )}
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Sign In
-        </Button>
-        <Button variant="outline" className="w-full">
-          Sign In with Google
-        </Button>
-        <div className="footer-links-login-form">
-          <a href="">Forgot your password?</a>
-          <p>
-            Don't have an account? <a href="">Sign up</a>
-          </p>
-        </div>
-      </CardFooter>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Sign In
+          </Button>
+          <Button variant="outline" className="w-full">
+            Sign In with Google
+          </Button>
+          <div className="footer-links-login-form">
+            <a href="">Forgot your password?</a>
+            <p>
+              Don't have an account? <a href="">Sign up</a>
+            </p>
+          </div>
+        </CardFooter>
+      </form>
     </Card>
   );
 };
